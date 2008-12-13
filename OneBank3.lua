1671a1dc07a733a8664df41e6b7cf2e241a7d0a0
Bryan McLemore <kaelten@gmail.com>
2008-12-13 14:14:10 -0600

    displays bank bags in the sidebar now

--- OneBank3.lua
+++ OneBank3.lua
@@ -9,6 +9,7 @@ function OneBank3:OnInitialize()
 	
 	self.displayName = "OneBank3"
 	self.core = OneCore3
+	self.isBank = true
 	
 	self.bagIndexes = {-1, 5, 6, 7, 8, 9, 10, 11}
 	
@@ -18,8 +19,6 @@ function OneBank3:OnInitialize()
 	self.frame:SetPosition(self.db.profile.position)
 	self.frame:CustomizeFrame(self.db.profile)
 	self.frame:SetSize(200, 200)
-	
-	self.Show = function() self.frame:Show() end
 	
 	self.frame:SetScript("OnShow", function()
 		if not self.frame.slots then
@@ -33,8 +32,10 @@ function OneBank3:OnInitialize()
 		local UpdateBag = function(event, bag) 
 			-- This is a work around of the fact that bank slots work different than all other slots.
 			if event == 'PLAYERBANKSLOTS_CHANGED' then
-				if ( bag <= NUM_BANKGENERIC_SLOTS ) then
-					bag = -1
+				if 	( bag <= NUM_BANKGENERIC_SLOTS ) then
+					BankFrameItemButton_Update(self.frame.slots["-1:"..bag])
+				else
+					BankFrameItemButton_Update(self.sidebar.buttons[bag-NUM_BANKGENERIC_SLOTS]);
 				end
 			end
 			
@@ -66,26 +67,41 @@ function OneBank3:OnInitialize()
 	self.frame.sidebar = self.sidebar
 	
 	self.sidebar:CustomizeFrame(self.db.profile)
-
---[[	
+	self.sidebar:SetHeight(4 * self.rowHeight + self.bottomBorder + self.topBorder) 
+	self.sidebar:SetWidth(2 * self.colWidth + self.leftBorder + self.rightBorder)
+	
+	
 	self.sidebar:SetScript("OnShow", function()
 		if not self.sidebar.buttons then
 			self.sidebar.buttons = {}
-			local button = self:GetBackbackButton(self.sidebar)
-			button:ClearAllPoints()
-			button:SetPoint("TOP", self.sidebar, "TOP", 0, -15)
 			
-			self.sidebar.buttons[-1] = button
-			for bag=0, 3 do
-				local button = self:GetBagButton(bag, self.sidebar)
-				button:ClearAllPoints()
-				button:SetPoint("TOP", self.sidebar, "TOP", 0, (bag + 1) * -31 - 10)
+			for row=1, 3 do
+				local b1ID, b2ID = row * 2 - 1, row * 2
+				local yOffset = 0 - 10 - ((row - 1) * self.rowHeight)
 				
-				self.sidebar.buttons[bag] = button
+				local button = self:GetBagButton(b1ID, self.sidebar)
+				button:ClearAllPoints()
+				button:SetPoint("TOPLEFT", self.sidebar, "TOPLEFT", self.leftBorder, yOffset)
+				self.sidebar.buttons[b1ID] = button
+				
+				local button2 = self:GetBagButton(b2ID, self.sidebar)
+				button2:ClearAllPoints()
+				button2:SetPoint("TOPLEFT", self.sidebar, "TOPLEFT", self.leftBorder + self.colWidth , yOffset)
+				self.sidebar.buttons[b2ID] = button2
 			end
+			
+			local button = self:GetBagButton(7, self.sidebar)
+			button:ClearAllPoints()
+			button:SetPoint("TOP", self.sidebar, "TOP", 0, 0 - 10 - (3 * self.rowHeight))
+			self.sidebar.buttons[7] = button	
+			
+			for _, button in pairs(self.sidebar.buttons) do
+				BankFrameItemButton_Update(button)
+			end
+			
 		end
+		self:UpdateBagSlotStatus()
 	end)
-	]]
 	
 	self.sidebar:Hide()
 	
@@ -159,4 +175,82 @@ function OneBank3:IsBagOpen(bag)
 	
 	return self.isOpened and bag or nil
 end
+
+function OneBank3:GetBagButton(bag, parent)
+
+	local button = CreateFrame("CheckButton", "OneBankSBBag"..bag, parent, 'BankItemButtonBagTemplate')
+	button:SetID(bag+4)
+	
+	--[[
+	self:SecureHookScript(button, "OnEnter", function(button)
+		self:HighlightBagSlots(button:GetID())
+	end)
+	
+	button:SetScript("OnLeave", function(button)
+		if not button:GetChecked() then
+			self:UnhighlightBagSlots(button:GetID())
+			self.frame.bags[button:GetID()].colorLocked = false
+		else
+			self.frame.bags[button:GetID()].colorLocked = true
+		end
+		GameTooltip:Hide()
+	end)
+	
+	button:SetScript("OnClick", function(button) 
+		local haditem = PutItemInBag(button:GetInventorySlot())
+
+		if haditem then
+			button:SetChecked(not button:GetChecked())
+		end 
+	end)
+	
+	button:SetScript("OnReceiveDrag", function(button) 
+		PutItemInBag(button:GetID())
+	end)
+	]]
+	
+	return button
+end
+
+function OneBank3:UpdateBagSlotStatus() 
+	--[[
+	local purchaseFrame = OBBBagFraPurchaseInfo
+	if( purchaseFrame == nil ) then
+		return
+	end
+	]]
+	
+	local numSlots,full = GetNumBankSlots()
+	local button
+	for i=1, NUM_BANKBAGSLOTS, 1 do
+		button = self.sidebar.buttons[i]
+		if ( button ) then
+			if ( i <= numSlots ) then
+				SetItemButtonTextureVertexColor(button, 1.0,1.0,1.0)
+				button.tooltipText = BANK_BAG
+			else
+				SetItemButtonTextureVertexColor(button, 1.0,0.1,0.1)
+				button.tooltipText = BANK_BAG_PURCHASE
+			end
+		end
+	end
+	--[[
+	-- pass in # of current slots, returns cost of next slot
+	local cost = GetBankSlotCost(numSlots)
+	BankFrame.nextSlotCost = cost
+	if( GetMoney() >= cost ) then
+		SetMoneyFrameColor("OBBBagFraPurchaseInfoDetailMoneyFrame", 1.0, 1.0, 1.0)
+	else
+		SetMoneyFrameColor("OBBBagFraPurchaseInfoDetailMoneyFrame", 1.0, 0.1, 0.1)
+	end
+	MoneyFrame_Update("OBBBagFraPurchaseInfoDetailMoneyFrame", cost)
+
+	if( full ) then
+		purchaseFrame:Hide()
+	else
+		purchaseFrame:Show()
+	end
+	]]
+end
+
 
